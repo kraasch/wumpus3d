e47a6c291047555773076dc8b67ca17c23ce68ee
Alex Kraasch <alex@kraasch.eu>
2024-01-05 11:33:22 +0100

    Setup more classes.

--- data/src/scenes/game.gd
+++ data/src/scenes/game.gd
@@ -4,13 +4,15 @@ const res_bomb = preload("res://data/src/scenes/bomb.tscn")
 const res_hole = preload("res://data/src/scenes/hole.tscn")
 const res_wump = preload("res://data/src/scenes/wumpus.tscn")
 const res_mush  = preload("res://data/src/scenes/bat.tscn")
+const res_play  = preload("res://data/src/scenes/player.tscn")
+const res_fog   = preload("res://data/src/scenes/fog.tscn")
 
-func place_object(resource, x, y):
+func place_object(resource, x, y, z : float = 0.25):
 	var offset : bool = true
 	var obj = resource.instantiate()
 	self.get_node('Objs').add_child(obj)
 	if offset:
-		obj.position.y = 0.25
+		obj.position.y = z
 	obj.position.z = -y
 	obj.position.x = x
 
@@ -19,3 +21,5 @@ func _ready():
 	place_object(res_wump, 3, 4)
 	place_object(res_hole, 4, 3)
 	place_object(res_mush, 4, 4)
+	place_object(res_play, 2, 2)
+	place_object(res_fog,  1, 1)
