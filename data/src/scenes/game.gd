901ae9f6833d1747d0f48f16b4c216c93c49f5d1
Alex Kraasch <alex@kraasch.eu>
2024-01-05 12:43:51 +0100

    Add marker class and rename mush to bat.

--- data/src/scenes/game.gd
+++ data/src/scenes/game.gd
@@ -3,12 +3,14 @@ extends Node3D
 const res_bomb = preload("res://data/src/scenes/bomb.tscn")
 const res_hole = preload("res://data/src/scenes/hole.tscn")
 const res_wump = preload("res://data/src/scenes/wumpus.tscn")
-const res_mush = preload("res://data/src/scenes/bat.tscn")
+const res_bat  = preload("res://data/src/scenes/bat.tscn")
 const res_play = preload("res://data/src/scenes/player.tscn")
 const res_fog  = preload("res://data/src/scenes/fog.tscn")
+const res_mark = preload("res://data/src/scenes/marker.tscn")
 
 var grid = []
 var view = []
+var mark = res_mark.instantiate()
 
 func place_object(resource, x, y, z : float = 0.25):
 	var offset : bool = true
@@ -20,6 +22,7 @@ func place_object(resource, x, y, z : float = 0.25):
 	obj.position.x = x
 
 func _ready():
+	mark.position.y = 0.25
 	new_game()
 	show_game()
 
@@ -40,7 +43,7 @@ func new_game():
 	grid = [
 			res_bomb, null, res_wump, res_hole,
 			null, null, null, null,
-			res_mush, null, res_play, null,
+			res_bat, null, res_play, null,
 			null, null, null, null,
 		]
 	view = [
@@ -49,4 +52,21 @@ func new_game():
 			false, false, true, false,
 			false, false, false, false,
 		]
-	
+
+func _input(event):
+	if event.is_action_pressed("ui_left"):
+		set_mark(0, 0)
+	if event.is_action_pressed("ui_right"):
+		set_mark(0, 1)
+	if event.is_action_pressed("ui_up"):
+		set_mark(1, 0)
+	if event.is_action_pressed("ui_down"):
+		set_mark(1, 1)
+
+var player_x = 3
+var player_y = 3
+
+func set_mark(x, y):
+	mark.position.x = player_x + x
+	mark.position.z = player_y + y
+	print('input')
